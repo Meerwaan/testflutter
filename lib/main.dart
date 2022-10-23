b586f97f089aef08e0ff1d4ecd8dca9859b222cb
Merwan <merwanlaouini@gmail.com>
2022-10-24 00:00:17 +0200

    Ajout d'un package externe

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'CiguEuro',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Bienvenue dans CiguEuro'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:  Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
