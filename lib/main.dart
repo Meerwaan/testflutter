88c43cd32989f855cec698c70454834f16b7752f
Merwan <merwanlaouini@gmail.com>
2022-10-24 01:43:43 +0200

    Ajout de style

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'CiguEuro',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.amberAccent,
+          foregroundColor: Colors.black,
+        )
+      ),
+      home: const RandomWords(),
     );
   }
 }
